aa5d5e650277d786bc687f2e25052d02acdfd079
atrgouti <bilalatrgouti2002@gmail.com>
2023-05-13 21:09:06 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from './NavBar'
 import MainSection from './MainSection'
 import AboutMe from './AboutMe'
 import MySkills from './MySkills'
-import Projects from './projects'
+import Projects from './Projects'
 import Contact from './Contact'
 import "./AppStyle.css"
 export default function App() {
