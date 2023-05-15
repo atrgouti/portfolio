cb34e35349009cc573817b9ed8c3aec0dea8ccbd
atrgouti <bilalatrgouti2002@gmail.com>
2023-05-15 21:05:04 +0200

    update

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -8,6 +8,9 @@ export default function Contact(){
                 <p>Feel free to reach out if you have any questions, think we could work together, or just want to connect ✌️</p>
                 <p className="email">atrgoutibilal@gmail.com</p>  
                 <p className="freelance">Freelance status: <span className="Available">Available for work</span></p>
+                <div className="social-media">
+                    
+                </div>
             </div>
         </div>
     )
